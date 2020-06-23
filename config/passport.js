08520888183891388a9c7d83c6a913b599d29238
AnirudhaRamteke <nganirudh2016.com>
2020-06-23 21:17:10 +0530

    added statement to check email varification

--- config/passport.js
+++ config/passport.js
@@ -15,6 +15,10 @@ module.exports = function(passport) {
           return done(null, false, { message: 'That email is not registered' });
         }
 
+        if(!user.confirmed){
+          return done(null, false, { message: 'Please verify your email before login' });
+        }
+
         // Match password
         bcrypt.compare(password, user.password, (err, isMatch) => {
           if (err) throw err;
