8832717494bda60b1959e5495bfb09c31ec1244c
AnirudhaRamteke <nganirudh2016.com>
2020-06-23 21:41:30 +0530

    minor changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'YOUR MONGODB DATABASE LINK TO CONNECT';
+dbPassword = 'mongodb+srv://AnirudhaRamtek:ani!72925@cluster0-pwws3.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
